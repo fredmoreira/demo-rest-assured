687b5b005c631a3c7da5f6fb434def1f0f288388
Frederico Moreira <fredericomoreira@MacFred.local>
2017-04-23 21:06:32 -0300

    Refactoring tests

--- src/test/java/br/com/demo/tests/api/DemoApiTestPhoneBook.java
+++ src/test/java/br/com/demo/tests/api/DemoApiTestPhoneBook.java
@@ -47,13 +47,13 @@ public class DemoApiTestPhoneBook {
 	{
 		given().
 		when().
-		get("contacts?name=Tester").
+			get("contacts?name=Tester").
 		then().
-		statusCode(200).
-		body("[0]._id", is("56d5efa8c82593800291c02b")).
-		body("[0].name", is("Tester")).
-		body("[0].mobilephone", is("0552188889999")).
-		body("[0].homephone", is("0552133332222"));
+			statusCode(200).
+			body("[0]._id", is("56d5efa8c82593800291c02b")).
+			body("[0].name", is("Tester")).
+			body("[0].mobilephone", is("0552188889999")).
+			body("[0].homephone", is("0552133332222"));
 	}
 
 	@Test
@@ -64,9 +64,21 @@ public class DemoApiTestPhoneBook {
 		when().
 			post("/contacts/").
 		then().
+			statusCode(201).
 			body("name", is("testerTDC")).
 			body("mobilephone", is("0313198888777")).
 			body("homephone", is("0313133334444"));
 	}
-
+	
+	@Test
+	public void postRequestFail() {
+		given().
+			contentType("application/json").
+			body("{\"name\":\"testerTDC\"}").
+		when().
+			post("/contacts/").
+		then().
+			statusCode(400).
+			body("", hasItem("Missing required property: mobilephone"));
+	}
 }
